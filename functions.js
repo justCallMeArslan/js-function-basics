b2a8fa712c12c52104c302f2ba006c613e91312b
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2025-12-23 15:32:44 +0300

    Complete parameters and arguments.

--- functions.js
+++ functions.js
@@ -58,3 +58,70 @@ function guessAnimal() {
 guessAnimal(); // Cockroach - used as a local variable
 
 console.log(animal); // Worm - ignored as global variable
+
+//Parameters
+
+//we can pass arbitrary data to functions using parameters, e.g function below has 2 parameters from and text:
+
+function showMessage3(from, text) {
+    console.log(from + ": " + text);
+}
+
+showMessage3("Ann", "Hi"); // when the function called, given values copied to local variables from and text
+showMessage3("Ann", "How are you doing?"); // given values called arguments as they get passed to the function
+
+// we declare functions listing their parameters, then call them passing arguments.
+
+
+//Default values
+
+// if function is called,but argument is not provided, then the corresponding value becomes undefined.
+
+showMessage3 ("Ann"); // in this example we call the same function as in previous example, but without giving 
+// one of arguments. result - "Ann" : undefined. 
+
+// we can specify the so-called "default" value for a parameter in the function declaration, using = :
+
+function showMessage4( from, text = "no text found"){
+    console.log(from + ": " + text);
+}
+
+showMessage4("Ann"); // Ann: no text found
+
+//default valuealso jumps in when parameter exists, but strictly equals to undefined:
+
+showMessage4("Ann", undefined); // Ann: no text found
+
+// in example above "no text given" is a string, butit can be a more complex expression, which is only eveluated
+// and assigned if the parameter is missing. in example below function anotherFunction() well be called, only 
+// if the text parameter is provided and will be called everytime when text parameter missing:
+
+function showMessage5(from, text = anotherFunction()){}
+
+
+
+// Alternative default parameters 
+
+// sometimes it makes sense to assign default values for parameters at a later stage after the function was 
+// already declared. 
+// we can check if the parameter is passed during function execution, by comparing it with undefined:
+
+function showMessage6(text){
+    if (text === undefined) { // check if parameter is missing 
+        text = "empty message";
+    }
+    console.log(text);
+}
+
+showMessage6(); //no arguments passed to give value to text parameter, resulted in "empty message" string
+
+// another option on modern JS engines is nullish coalescing operator ??, its better when most falsy values,
+// such as 0, should be considered "normal" :
+
+function showCount(count) {
+    console.log(count ?? "unknown"); // if count undefiend or null, show "unknown"
+}
+
+showCount(0);
+showCount(null);
+showCount();
